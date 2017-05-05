621b8c22dbdab8c46bda8d32f170c349989616e3
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-05 10:53:58 -0700

    Add singleton features

--- src/com/sevebadajoz/stroketiming/controller/Controller.java
+++ src/com/sevebadajoz/stroketiming/controller/Controller.java
@@ -1,4 +1,18 @@
 package com.sevebadajoz.stroketiming.controller;
 
 public class Controller {
+	private static Controller mOne = null;
+
+	private static final String DB_NAME = "geedorah";
+
+
+	private Controller() {}
+	private Controller getInstance() {
+		if(mOne == null) {
+
+		}
+		return mOne;
+	}
+
+
 }
