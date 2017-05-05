fc0c50478f775a9ad66cd21023b9d80d4fa39b53
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-05 11:06:22 -0700

    Add functionality

--- src/com/sevebadajoz/stroketiming/model/Athlete.java
+++ src/com/sevebadajoz/stroketiming/model/Athlete.java
@@ -1,4 +1,14 @@
 package com.sevebadajoz.stroketiming.model;
 
-public abstract class Athlete {
+public class Athlete {
+	private String mName;
+	private int mWeight;
+
+	int getWeight() {
+		return mWeight;
+	}
+	String getName() {
+		return mName;
+	}
+
 }
