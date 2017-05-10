c61a895d4a7b3da773bc817219776b436871cea7
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-09 21:49:37 -0700

    Alter constructor

--- src/com/sevebadajoz/stroketiming/model/Rower.java
+++ src/com/sevebadajoz/stroketiming/model/Rower.java
@@ -1,7 +1,10 @@
 package com.sevebadajoz.stroketiming.model;
 
 public class Rower extends Athlete {
-	protected Rower(String name, double weight) {
+	ErgScore mErgScore;
+	int mPosition;
+	protected Rower(String name, double weight, ErgScore ergScore) {
 		super(name, weight);
+		mErgScore = ergScore;
 	}
 }
