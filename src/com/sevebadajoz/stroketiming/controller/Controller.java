059082dc3d15ff96f0ba610edf1580f617b058af
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-09 22:02:38 -0700

    Change Controller.java to singleton

--- src/com/sevebadajoz/stroketiming/controller/Controller.java
+++ src/com/sevebadajoz/stroketiming/controller/Controller.java
@@ -1,18 +1,12 @@
 package com.sevebadajoz.stroketiming.controller;
 
 public class Controller {
-	private static Controller mOne = null;
+	private static Controller ourInstance = new Controller();
 
-	private static final String DB_NAME = "geedorah";
-
-
-	private Controller() {}
-	private Controller getInstance() {
-		if(mOne == null) {
-
-		}
-		return mOne;
+	public static Controller getInstance() {
+		return ourInstance;
 	}
 
-
+	private Controller() {
+	}
 }
