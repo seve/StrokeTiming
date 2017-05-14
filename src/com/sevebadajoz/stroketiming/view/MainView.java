03dd44917281c8ee3b617a82d0a36636a8db8a50
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-14 14:30:58 -0700

    Create workable UI

--- src/com/sevebadajoz/stroketiming/view/MainView.java
+++ src/com/sevebadajoz/stroketiming/view/MainView.java
@@ -3,10 +3,15 @@ package com.sevebadajoz.stroketiming.view;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class MainView extends Application implements {
+public class MainView extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        ViewSwitch.setStage(primaryStage);
+        ViewSwitch.loadScene("Stroke Timing", ViewSwitch.MAIN_SCENE);
     }
 }
--- src/com/sevebadajoz/stroketiming/view/ViewSwitch.java
+++ src/com/sevebadajoz/stroketiming/view/ViewSwitch.java
@@ -1,4 +1,29 @@
 package com.sevebadajoz.stroketiming.view;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ViewSwitch {
+    public static final String MAIN_SCENE = "main.fxml";
+    public static Stage activeStage;
+
+    public static void setStage(Stage stage) {
+        activeStage = stage;
+    }
+
+    public static void loadScene(String title, String sceneFXML) {
+        try {
+            activeStage.setTitle(title);
+            Scene scene = new Scene(FXMLLoader.load(ViewSwitch.class.getClassLoader().getResource(sceneFXML)));
+            activeStage.setScene(scene);
+            activeStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
+
