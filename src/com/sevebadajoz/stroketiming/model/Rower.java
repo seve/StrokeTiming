588b2a99ea5cd383de0f7f98b765386b70e5065f
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-13 22:48:52 -0700

    Add functionality

--- src/com/sevebadajoz/stroketiming/model/Rower.java
+++ src/com/sevebadajoz/stroketiming/model/Rower.java
@@ -1,4 +1,28 @@
 package com.sevebadajoz.stroketiming.model;
 
 public class Rower extends Athlete {
+    protected ErgScore mErgScore;
+    protected int mInches;
+
+    public Rower(String name, double weight, ErgScore ergScore, int inches) {
+        super(name, weight);
+        mErgScore = ergScore;
+        mInches = inches;
+    }
+
+    public ErgScore getErgScore() {
+        return mErgScore;
+    }
+
+    public void setErgScore(ErgScore ergScore) {
+        mErgScore = ergScore;
+    }
+
+    public int getInches() {
+        return mInches;
+    }
+
+    public void setInches(int inches) {
+        mInches = inches;
+    }
 }
