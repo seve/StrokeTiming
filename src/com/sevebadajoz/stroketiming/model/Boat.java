3648d59025ce5c46b7e640f6181b5528590cbc6f
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-13 22:58:00 -0700

    Add functionality

--- src/com/sevebadajoz/stroketiming/model/Boat.java
+++ src/com/sevebadajoz/stroketiming/model/Boat.java
@@ -3,5 +3,49 @@ package com.sevebadajoz.stroketiming.model;
 public class Boat {
     protected String mName;
     protected int mSeats;
+    protected int mWeight;
+    protected String mMake;
 
+    public Boat(String name, int seats, String make) {
+        mName = name;
+        mSeats = seats;
+        mMake = make;
+    }
+
+    public Boat(String name, int seats, int weight, String make) {
+        this(name, seats, make);
+        mMake = make;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public int getSeats() {
+        return mSeats;
+    }
+
+    public void setSeats(int seats) {
+        mSeats = seats;
+    }
+
+    public int getWeight() {
+        return mWeight;
+    }
+
+    public void setWeight(int weight) {
+        mWeight = weight;
+    }
+
+    public String getMake() {
+        return mMake;
+    }
+
+    public void setMake(String make) {
+        mMake = make;
+    }
 }
