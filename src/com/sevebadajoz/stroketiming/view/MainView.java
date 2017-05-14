68a662a311cfb65858561db08398ffd4efeda843
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-14 13:35:26 -0700

    Create MainView.java

---
+++ src/com/sevebadajoz/stroketiming/view/MainView.java
@@ -0,0 +1,12 @@
+package com.sevebadajoz.stroketiming.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainView extends Application implements {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
