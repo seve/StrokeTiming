b07076d40d1ab79ba7b6177bde56209f400d14c8
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-16 20:29:22 -0700

    Create BoatListScene.java

---
+++ src/com/sevebadajoz/stroketiming/view/BoatListScene.java
@@ -0,0 +1,31 @@
+package com.sevebadajoz.stroketiming.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BoatListScene extends Application implements Initializable {
+
+	@FXML
+	private Button mAddButton;
+	@FXML
+	private Button mExitButton;
+	@FXML
+	private ListView mBoatList;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+	}
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+
+	}
+}
