5cd318305b9d3849eb846bf0ffb044073da2b575
Kenia <kdrioja@gmail.com>
2017-05-23 10:15:19 -0700

    Added field names and field types to Controller table names

--- src/com/sevebadajoz/stroketiming/controller/Controller.java
+++ src/com/sevebadajoz/stroketiming/controller/Controller.java
@@ -9,13 +9,52 @@ import javafx.collections.ObservableList;
 
 public class Controller {
 	private static final String DB_NAME = "stroke_timing.db";
+
 	private static final String BOATS_TABLE_NAME = "boats";
+	private static final String[] BOATS_FIELD_NAMES = {"id", "name", "type", "weight", "seats"};
+	private static final String[] BOATS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "TEXT", "TEXT", "REAL", "INTEGER"};
+
 	private static final String BOAT_LINEUPS_TABLE_NAME = "boat_lineups";
+	private static final String[] BOAT_LINEUPS_FIELD_NAMES = {"id", "coxswain_id", "stroke_seat_id", "seat_two_id", "seat_three_id",
+			"seat_four_id", "seat_five_id", "seat_six_id", "seat_seven_id", "bow_seat_id"};
+	private static final String[] BOAT_LINEUPS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "INTEGER", "INTEGER", "INTEGER", "INTEGER", "INTEGER",
+			"INTEGER", "INTEGER", "INTEGER", "INTEGER"};
+
+	private static final String BOATS_TO_BOAT_LINEUPS_TABLE_NAME = "boats_to_boat_lineups";
+	private static final String[] BOATS_TO_BOAT_LINEUPS_FIELD_NAMES = {"boat_id", "boat_lineup_id"};
+	private static final String[] BOATS_TO_BOAT_LINEUPS_FIELD_TYPES = {"INTEGER", "INTEGER"};
+
 	private static final String ROWERS_TABLE_NAME = "rowers";
+	private static final String[] ROWERS_FIELD_NAMES = {"id", "name", "position", "weight"};
+	private static final String[] ROWERS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "TEXT", "INTEGER", "REAL"};
+
+	private static final String ERG_SCORES_TABLE_NAME = "erg_scores";
+	private static final String[] ERG_SCORES_FIELD_NAMES = {"id", "seconds", "meters", "weight"};
+	private static final String[] ERG_SCORES_FIELD_TYPES = {"INTEGER PRIMARY KEY", "REAL", "INTEGER", "REAL"};
+
+	private static final String ROWER_ERG_SCORES_TABLE_NAME = "rower_erg_scores";
+	private static final String[] ROWER_ERG_SCORES_FIELD_NAMES = {"rower_id", "erg_score_id"};
+	private static final String[] ROWER_ERG_SCORES_FIELD_TYPES = {"INTEGER", "INTEGER"};
+
 	private static final String COXSWAINS_TABLE_NAME = "coxswains";
-    private static final String ERG_SCORES_TABLE_NAME = "erg_scores";
+	private static final String[] COXSWAINS_FIELD_NAMES = {"id", "name", "weight"};
+	private static final String[] COXSWAINS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "TEXT", "REAL"};
+
     private static final String WORKOUTS_TABLE_NAME = "workouts";
+	private static final String[] WORKOUTS_FIELD_NAMES = {"id", "date", "comments"};
+	private static final String[] WORKOUTS_FIELD_TYPES = {"INTEGER PRIMARY KEY", "TEXT", "TEXT"};
+
+	private static final String BOAT_WORKOUTS_TABLE_NAME = "boat_workouts";
+	private static final String[] BOAT_WORKOUTS_FIELD_NAMES = {"boat_id", "workout_id"};
+	private static final String[] BOAT_WORKOUTS_FIELD_TYPES = {"INTEGER", "INTEGER"};
+
     private static final String PIECES_TABLE_NAME = "pieces";
+	private static final String[] PIECES_FIELD_NAMES = {"id", "type", "meters", "time", "comments"};
+	private static final String[] PIECES_FIELD_TYPES = {"INTEGER PRIMARY KEY", "TEXT", "INTEGER", "REAL", "TEXT"};
+
+	private static final String WORKOUT_TO_PIECES_TABLE_NAME = "workout_to_pieces";
+	private static final String[] WORKOUT_TO_PIECES_FIELD_NAMES = {"workout_id", "pieces_id"};
+	private static final String[] WORKOUT_TO_PIECES_FIELD_TYPES = {"INTEGER", "INTEGER"};
 
     private static Controller theOne;
 
@@ -24,9 +63,7 @@ public class Controller {
     private ObservableList<Coxswain> mCoxswains;
     private ObservableList<Rower> mRowers;
 
-
-	private Controller() {
-	}
+	private Controller() { }
 
     public static Controller getInstance() {
         if (theOne == null) {
