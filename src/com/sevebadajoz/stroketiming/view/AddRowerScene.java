105d70a4597c22318da0f1ff08b27be97491d681
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-23 11:58:03 -0700

    Initialize sized

--- src/com/sevebadajoz/stroketiming/view/AddRowerScene.java
+++ src/com/sevebadajoz/stroketiming/view/AddRowerScene.java
@@ -2,10 +2,15 @@ package com.sevebadajoz.stroketiming.view;
 
 import com.sevebadajoz.stroketiming.controller.Controller;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-public class AddRowerScene {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddRowerScene implements Initializable {
     static Controller mController = Controller.getInstance();
     @FXML
     TextField mName;
@@ -15,4 +20,16 @@ public class AddRowerScene {
     Button mBack;
     @FXML
     Button mAdd;
+    @FXML
+    ComboBox<String> mSides;
+    @FXML
+    TextField mDistance;
+    @FXML
+    TextField mTime;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        mSides.getItems().addAll("Port", "Starboard", "Both");
+    }
 }
