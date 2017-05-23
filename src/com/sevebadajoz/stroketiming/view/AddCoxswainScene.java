112da8d10985ea503ec4a58af1e27d13bbbc0ead
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-23 11:13:32 -0700

    Add functionality to AddCoxswainScene.java

--- src/com/sevebadajoz/stroketiming/view/AddCoxswainScene.java
+++ src/com/sevebadajoz/stroketiming/view/AddCoxswainScene.java
@@ -1,4 +1,39 @@
 package com.sevebadajoz.stroketiming.view;
 
+import com.sevebadajoz.stroketiming.controller.Controller;
+import com.sevebadajoz.stroketiming.model.Coxswain;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class AddCoxswainScene {
+    static Controller mController = Controller.getInstance();
+    @FXML
+    TextField mName;
+    @FXML
+    TextField mWeight;
+    @FXML
+    Button mBack;
+    @FXML
+    Button mAdd;
+
+    @FXML
+    Object back() {
+        clear();
+        ViewSwitch.loadScene("Add a Lineup", ViewSwitch.ADD_LINEUP_SCENE);
+        return this;
+    }
+
+    @FXML
+    Object add() {
+        mController.getCoxswains().add(new Coxswain(mName.getText().trim(), Double.parseDouble(mWeight.getText())));
+        ViewSwitch.loadScene("Add a Lineup", ViewSwitch.ADD_LINEUP_SCENE);
+        return this;
+    }
+
+    void clear() {
+        mName.clear();
+        mWeight.clear();
+    }
 }
