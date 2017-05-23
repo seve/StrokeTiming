f5e0fcc898e14d43647bc91ac30280065cf7da8b
Severiano Badajoz <sevebadajoz@gmail.com>
2017-05-22 20:37:31 -0700

    Make getters and setters

--- src/com/sevebadajoz/stroketiming/model/Practice.java
+++ src/com/sevebadajoz/stroketiming/model/Practice.java
@@ -5,4 +5,11 @@ import java.util.ArrayList;
 public class Practice {
     ArrayList<WaterPiece> mWaterPieces;
 
+    public ArrayList<WaterPiece> getWaterPieces() {
+        return mWaterPieces;
+    }
+
+    public void setWaterPieces(ArrayList<WaterPiece> waterPieces) {
+        mWaterPieces = waterPieces;
+    }
 }
